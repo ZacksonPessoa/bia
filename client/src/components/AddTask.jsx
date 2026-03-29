3a16029944c7469b1817468d6f910cee31fcd95c
Zackson Pessoa <zmppesso2@gmail.com>
2026-03-29 13:45:03 +0000

    ajuste no texto do botao

--- client/src/components/AddTask.jsx
+++ client/src/components/AddTask.jsx
@@ -59,7 +59,7 @@ const AddTask = ({ onAdd }) => {
       </div>
       
       <button type="submit" className="btn btn-block success">
-        Adiconar Nova Tarefa
+        Adicionar Nova Tesk
       </button>
       
       <Modal
