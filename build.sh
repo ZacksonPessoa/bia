2240996a7119f7eb6e9d7e17d34dc3b6b35c984c
Zackson Pessoa <zacksonpessoa@hotmail.com>
2026-04-04 23:51:49 +0000

    fix: update repo bia

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-ECR_REGISTRY="380093117576.dkr.ecr.us-east-1.amazonaws.com"
+ECR_REGISTRY="216773127050.dkr.ecr.us-east-1.amazonaws.com"
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $ECR_REGISTRY
 docker build -t bia .
 docker tag bia:latest $ECR_REGISTRY/bia:latest
